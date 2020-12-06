831dc858daefa9efe5360192419298c35b6408e7
Shira Segev <shirasegev@gmail.com>
2020-12-06 19:09:28 +0200

    Shorter code with no duplicates

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import '../App.scss';
 
 export type FilterState = {
-    selectedFulfillmentOption: string,
-	selectedPaymentOption: string,
+    selectedFilter: string,
 };
 
 type Props = {
-    onFulfillmentFilter: Function,
-    onPaymentFilter: Function;
+    onFilterChange: Function;
+    values: string[][];
 };
 
 export class FilterComponent extends React.Component<Props, FilterState> {
@@ -16,73 +15,27 @@ export class FilterComponent extends React.Component<Props, FilterState> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            selectedFulfillmentOption: '',
-            selectedPaymentOption: '',
+            selectedFilter: '',
         };
     }
 
     // Part B 3
     render () {
         return (
-            <div className={'filters'}>
-                <div className={'filter'}>
-                    <h4>Fulfillment filter: </h4>
-                    {this.showFulfillmentFilterOptions()}
-                </div>
-                <div className={'filter'}>
-                    <h4>Payment filter: </h4>
-                    {this.showPaymentFilterOptions()}
-                </div>
-            </div>	
+            <div>
+                <select className={'selectpicker'} onChange={(e) => this.onFilterChange(e.target.value)}>
+                    {this.props.values.map((option) => 
+                        <option value={option[0]} key={option[0]}>{option[1]}</option>
+                    )}
+                </select>
+            </div>
         );
     }
 
-    // // Part B 3
-    // render () {
-    //     return (
-    //         <div className={'filter'}>
-    //             <select className={'selectpicker'} onChange={(e) => this.handleFilterChanged(e.target.value)}>
-    //                 this.props.values.keys.map((key) => {
-    //                     <option value={key}>{this.props.values[key]}</option>
-    //                 })
-    //             </select>
-    //         </div>
-    //     );
-    // }
-
-    showFulfillmentFilterOptions = () => {
-        return (
-            <select className={'selectpicker'} onChange={(e) => this.handleFulfillmentOptionChange(e.target.value)}>
-                <option value=''>				All orders				</option>
-                <option value='fulfilled'>		Delivered orders		</option>
-                <option value='not-fulfilled'>	Not delivered orders	</option>
-                <option value='canceled'>		Canceled orders			</option>
-            </select>
-        )
-    };
-
-    handleFulfillmentOptionChange = (fulfillmentFilter: string) => {
-        this.props.onFulfillmentFilter(fulfillmentFilter);
+    onFilterChange = (selectedFilter: string) => {
+        this.props.onFilterChange(selectedFilter);
         this.setState({
-            selectedFulfillmentOption: fulfillmentFilter,
-        });
-    }
-
-    showPaymentFilterOptions = () => {
-        return (
-            <select className={'selectpicker'} onChange={(e) => this.handlePaymentOptionChange(e.target.value)}>
-                <option value=''>			All orders			</option>
-                <option value='paid'>		Paid orders			</option>
-                <option value='not-paid'>	Not paid orders		</option>
-                <option value='refunded'>	Refunded orders		</option>
-            </select>
-        )
-    };
-
-    handlePaymentOptionChange = (paymentFilter: string) => {
-        this.props.onPaymentFilter(paymentFilter);
-        this.setState({
-            selectedPaymentOption: paymentFilter,
+            selectedFilter: selectedFilter,
         });
     }
 }
